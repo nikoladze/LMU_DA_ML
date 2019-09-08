5f5cd33152d0d98a4e9079d9a828000604ef64b2
Nikolai Hartmann <Nikolai.Hartmann@physik.uni-muenchen.de>
2019-09-08 13:50:32 +0200

    update HiggsChallenge notebooks
    * switch to AMS scans instead of fixed quantile cut
    
    * set number of cores to 1 for keras example
    
    * activate early stopping in sklearn MLP examples

--- ams.py
+++ ams.py
@@ -1,5 +1,4 @@
-def ams(s,b):
-    from math import sqrt,log
-    if b==0:
-        return 0
-    return sqrt(2*((s+b+10)*log(1+float(s)/(b+10))-s))
+def ams(s, b):
+    import numpy as np
+    return np.sqrt(2*((s+b+10)*np.log(1+s/(b+10))-s))
+
