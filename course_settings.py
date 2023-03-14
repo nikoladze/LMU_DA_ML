0ca00ae9ef35e546816584ba5297f7c623d00e70
Nikolai Hartmann <Nikolai.Hartmann@physik.uni-muenchen.de>
2023-03-14 18:45:10 +0100

    adjust higgs notebooks and course_settings.py to work well on CIP GPUs

--- course_settings.py
+++ course_settings.py
@@ -4,6 +4,7 @@ def set_tf_nthreads(nthreads):
     import os
     os.environ["OMP_NUM_THREADS"] = str(nthreads)
     os.environ["MKL_NUM_THREADS"] = str(nthreads)
+    os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
     import tensorflow as tf
     tf.config.threading.set_intra_op_parallelism_threads(nthreads)
     tf.config.threading.set_inter_op_parallelism_threads(nthreads)
